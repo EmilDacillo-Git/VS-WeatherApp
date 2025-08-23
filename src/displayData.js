a698ae110676adea09143a1230819805e088d99f
EmilDacillo-Git <dacilloemil09@gmail.com>
2025-08-24 02:03:34 +0800

    started building the backend pipeline

--- src/displayData.js
+++ src/displayData.js
@@ -1,23 +1,30 @@
 import getData from "./weatherData.js";
+import sunIcon from "./assets/sun.png";
 
-async function weatherDatas(location) {
+const weatherPage = document.getElementById("weather-content");
+
+export default async function weatherDatas(location) {
   const fetchedData = await getData(location);
-
   const currentTemp = fetchedData.currentConditions.temp;
-  console.log(currentTemp);
+
+  const tempToday = document.createElement("h1");
+  const tempIcon = document.createElement('img');
+  tempIcon.src = sunIcon; 
+  tempIcon.alt = "Weather Icon";
+  tempIcon.classList.add('temp-icon')
   
+  tempToday.classList.add("temp-today");
+  tempToday.append(currentTemp);
+  weatherPage.append(tempIcon,tempToday);
 
+  // console.log(currentTemp);
 
+  // const curretnPrecipitation = fetchedData.days[0].precip;
+  // console.log(curretnPrecipitation);
 
+  // const currentWind = fetchedData.currentConditions.windspeed;
+  // console.log(currentWind);
 
-  const curretnPrecipitation = fetchedData.days[0].precip;
-  console.log(curretnPrecipitation);
-
-  const currentWind = fetchedData.currentConditions.windspeed;
-  console.log(currentWind);
-
-  const currentHumidity = fetchedData.currentConditions.humidity;
-  console.log(currentHumidity);
+  // const currentHumidity = fetchedData.currentConditions.humidity;
+  // console.log(currentHumidity);
 }
-
-weatherDatas("manila");
--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
+import weatherDatas from "./displayData.js";
 import "./styles.css";
 //  Key: UHW5LXTHFPBQ3AJ5QPDV56PLF
+
+const locationInput = document.getElementById('location');
+const heroPage = document.getElementById('hero-page');
+const searchBtn = document.getElementById('search-location');
+const form = document.getElementById('locationForm');
+const weatherDisplay = document.getElementById('weather-content');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = locationInput.value.trim();
+
+    if (location) {
+        weatherDisplay.innerHTML = "";
+        heroPage.style.display = 'none';
+        weatherDatas(location);
+    }
+    
+});
 
 
